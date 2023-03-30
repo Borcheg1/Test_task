f8395581cdd4365123af7e3b82ca65d46313b381
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-03-30 16:39:08 +0300

    update database.py (created class DatabasePostgres with methods create_table and get_all_data)

---
+++ database.py
@@ -0,0 +1,58 @@
+"""
+Используется база данных PostgreSQL
+Класс, отвечающий за все запросы к базе данных
+"""
+
+import os
+
+import psycopg2
+
+
+class DatabasePostgres:
+    def __init__(self):
+        """
+        Конструктор класса
+
+        Для обеспечения коннекта к базе данных задайте следующие
+        переменные окружения в файле .env
+
+        PGDATABASE: str, название базы данных, например PGDATABASE=google_sheets
+        PGUSER: str, имя пользователя, например PGUSER=postgres
+        PGPASSWORD: str, пароль к базе данных, например PGPASSWORD=123
+        PGHOST: str, хост базы данных, например PGHOST=localhost
+        PGPORT: str, порт базы данных, например PGPORT=5432
+        """
+
+        self.connection = psycopg2.connect(
+            database=os.environ["PGDATABASE"],
+            user=os.environ["PGUSER"],
+            password=os.environ["PGPASSWORD"],
+            host=os.environ["PGHOST"],
+            port=os.environ["PGPORT"]
+        )
+        self.cursor = self.connection.cursor()
+
+    def create_table(self):
+        """
+        Создание таблицы, если таблицы с таким названием не существует
+        :return: None
+        """
+
+        with self.connection:
+            self.cursor.execute(
+                "CREATE TABLE IF NOT EXISTS google_sheet ("
+                "№ serial PRIMARY KEY,"
+                "Заказ № INTEGER UNIQUE,"
+                "Стоимость,$ INTEGER,"
+                "Срок поставки VARCHAR (15)"
+            )
+
+    def get_all_data(self):
+        """
+        Получение всех данных из таблицы
+
+        :return: list[tuple], лист с кортежами данных каждой строки таблицы
+        """
+        with self.connection:
+            self.cursor.execute("SELECT * FROM qooqle_sheet")
+        return self.cursor.fetchall()
