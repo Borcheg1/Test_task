2dd9243f93816b1cbcefd30cd189029bb0ae67d9
Dmitriy Borozdin <Borozdin.do1@gmail.com>
2023-03-31 17:22:25 +0300

    update database.py (renamed class DatabasePostgres to GoogleSheetDatabase; renamed func create_table to create_sheet_table; updated func create_sheet_table; updated func update_table; added class TelegramDatabase)

--- database.py
+++ database.py
@@ -8,7 +8,7 @@ import os
 import psycopg2
 
 
-class DatabasePostgres:
+class GoogleSheetDatabase:
     def __init__(self):
         """
         Конструктор класса
@@ -32,9 +32,9 @@ class DatabasePostgres:
         )
         self.cursor = self.connection.cursor()
 
-    def create_table(self):
+    def create_sheet_table(self):
         """
-        Запрос на создание таблицы, если таблицы с таким названием не существует
+        Запрос на создание таблицы основаной на google sheet, если таблицы с таким названием не существует
         :return: None
         """
 
@@ -44,7 +44,8 @@ class DatabasePostgres:
                 '№ INTEGER PRIMARY KEY,'
                 '"Заказ №" INTEGER UNIQUE,'
                 '"Стоимость,$" INTEGER,'
-                '"Срок поставки" VARCHAR (15))'
+                '"Срок поставки" VARCHAR (15),'
+                '"Стоимость в руб." NUMERIC (10, 2))'
             )
 
     def get_all_data(self):
@@ -57,7 +58,7 @@ class DatabasePostgres:
             self.cursor.execute("SELECT * FROM google_sheet")
         return self.cursor.fetchall()
 
-    def update_table(self, row):
+    def update_table(self, row, rub):
         """
         Запрос на добавление строки в таблицу
 
@@ -68,8 +69,9 @@ class DatabasePostgres:
 
         with self.connection:
             self.cursor.execute(
-                'INSERT INTO google_sheet (№, "Заказ №", "Стоимость,$", "Срок поставки") VALUES (%s, %s, %s, %s)',
-                (int(row[0]), int(row[1]), int(row[2]), row[3])
+                'INSERT INTO google_sheet (№, "Заказ №", "Стоимость,$", "Срок поставки", "Стоимость в руб.")'
+                'VALUES (%s, %s, %s, %s, %s)',
+                (int(row[0]), int(row[1]), int(row[2]), row[3], rub)
             )
 
     def delete_all_rows(self):
@@ -80,3 +82,53 @@ class DatabasePostgres:
         """
         with self.connection:
             self.cursor.execute("DELETE FROM google_sheet WHERE № > 0")
+
+
+class TelegramDatabase(GoogleSheetDatabase):
+    def create_telegram_table(self):
+        """
+        Запрос на создание таблицы пользователей телеграм, если таблицы с таким названием не существует
+        :return: None
+        """
+
+        with self.connection:
+            self.cursor.execute(
+                'CREATE TABLE IF NOT EXISTS telegram_id ('
+                '№ serial PRIMARY KEY,'
+                'user_id INTEGER UNIQUE)'
+            )
+
+    def add_user(self, user_id):
+        """
+        Добавление id пользователя.
+        :param user_id: int, id пользователя
+        :return: None
+        """
+
+        with self.connection:
+            self.cursor.execute("INSERT INTO telegram_id (user_id) VALUES (%s)", (user_id,))
+
+    def check_user_exist(self, user_id):
+        """
+        Проверка есть ли пользователь в БД
+
+        :param user_id: int, id пользователя
+        :return: bool
+        """
+
+        with self.connection:
+            self.cursor.execute("SELECT user_id FROM telegram_id WHERE user_id = (%s)", (user_id,))
+            return bool(len(self.cursor.fetchall()))
+
+    def get_users_ids(self):
+        """
+        Получение id всех пользователей.
+
+        :return: list[tuple], лист с кортежами, в которых user_id: int
+        """
+
+        with self.connection:
+            self.cursor.execute("SELECT user_id FROM telegram_id")
+            return self.cursor.fetchall()
+
+
